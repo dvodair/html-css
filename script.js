8ac1681dee22284f5c2b095148f95c106d6f1db5
Odair <163183558+dvodair@users.noreply.github.com>
2024-03-13 21:30:14 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+
+let num = document.querySelector('input#fnum')
+
+let lista = document.querySelector('select#flista')
+
+let res = document.querySelector('div#res')
+
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 200) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1)  {//  o indexOf  a letra (O) tem que ser com letra maiuscúlas//
+        return true
+    } else {
+        return false
+    }
+
+}
+
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value,valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado. `
+        lista.appendChild(item)
+        res.innerHTML = " "
+
+    } else {
+        window.alert('valor inválido ou já encontrado na lista')
+    }
+
+    num.value = " "
+    num.focus()
+
+}
+
+
+
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de Finalizar.')
+    } else {
+        let total = valores.length
+        let maior = valores [0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        
+        }
+
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo,temo ${total} números cadastrado. </p>`
+        res.innerHTML += `<p> o maior valor informado foi  ${maior} </p>`
+        res.innerHTML += `<p> o menor valor informado foi ${menor}   </p>`
+        res.innerHTML += `<p> somando todos  os valores, temos ${soma}. </p>`
+        res.innerHTML += `<p> a media dos valores digitado é ${media}`
+
+    
+    }
+}
